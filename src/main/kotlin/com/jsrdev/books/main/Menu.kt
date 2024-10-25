fd620e79a30a2f4a33e9a092ecebeb596a986fd0
Jose SR <josesanchez09190@gmail.com>
2024-10-25 01:43:09 -0600

    added search by title book

--- src/main/kotlin/com/jsrdev/books/main/Menu.kt
+++ src/main/kotlin/com/jsrdev/books/main/Menu.kt
@@ -4,6 +4,8 @@ import com.jsrdev.books.models.BookData
 import com.jsrdev.books.models.ResultData
 import com.jsrdev.books.service.DeserializeBookData
 import com.jsrdev.books.service.GetBookData
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 const val BASE_URL = "https://gutendex.com/"
 
@@ -13,14 +15,57 @@ class Menu(
 ) {
 
     fun showMenu() {
-        println("Data: ${getResultData()}")
+        val json: String = getStringData()
+        println("Json Data: $json")
 
-        val resultData: List<BookData> = resultData().results
+        val resultData: List<BookData> = resultData(json).results
 
+        // show randoms books
         val booksToShow = 5
         showBooks(booksToShow, resultData)
 
+        // top 10 most downloaded books
         showTop10MostDownloadedBooks(resultData)
+
+        // search by title book
+        showByTitleBook()
+
+    }
+
+    private fun showByTitleBook() {
+        var titleBook = getTitleBook()
+        while (titleBook.isNullOrEmpty()) {
+            println("Invalid entry, please, try again")
+            titleBook = getTitleBook()
+        }
+
+        titleBook = titleBook.trim().lowercase()
+        titleBook = encodedAndFormatTitleBook(titleBook)
+
+        val json: String = getStringData(buildUrl(title = titleBook))
+        val resultDataByTitle: List<BookData> = resultData(json).results
+
+        val book: BookData? = resultDataByTitle.asSequence()
+            .filter { it.languages.any { lang -> lang.equals("ES", ignoreCase = true)  }}
+            .filter { it.title.contains(titleBook, ignoreCase = true) }
+            .firstOrNull()
+
+        println()
+        return book?.let {
+            println("Libro encontrado")
+            println("$book")
+        } ?: println("No encontre ninguna coincidencia con: $titleBook")
+    }
+
+    private fun encodedAndFormatTitleBook(title: String): String {
+        // encodedSeriesName.replace("+", "%20")
+        return URLEncoder.encode(title, StandardCharsets.UTF_8)
+    }
+
+    private fun getTitleBook(): String? {
+        println()
+        println("Excribe el nombre del libro a consultar: ")
+        return readlnOrNull()
     }
 
     private fun showTop10MostDownloadedBooks(bookList: List<BookData>) {
@@ -51,21 +96,24 @@ class Menu(
 
         return bookList.asSequence()
             .drop(startIndex) // Saltamos hasta el índice inicial calculado
-            .take(booksToShow) // Tomamos los libros necesarios
+            .take(booksToShow)
             .forEachIndexed { ind, book ->
                 println("${ind + 1}: $book")
             }
     }
 
-    private fun resultData(): ResultData = deserializeData.getData(
-        json = getResultData(), genericClass = ResultData::class.java
-    )
+    private fun resultData(json: String): ResultData =
+        deserializeData.getData(json , ResultData::class.java)
 
-    private fun getResultData(): String {
-        val url = "${BASE_URL}books/"
-        return getStringData(url = url)
-    }
-
-    private fun getStringData(url: String): String =
+    private fun getStringData(url: String = buildUrl(title = null)): String =
         apiService.getData(url)
+
+    private fun buildUrl(title: String?): String {
+
+        val urlBuilder: StringBuilder = StringBuilder(BASE_URL)
+        urlBuilder.append("books/")
+        title?.let { urlBuilder.append("?search=${title}") }
+
+        return urlBuilder.toString()
+    }
 }
